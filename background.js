db775344497ce35d2dedf76c3116ee1077f770e9
Omar Stewey <rush0g@gmail.com>
2013-01-26 11:43:24 -0500

    Updated psuedo logic: -Page should check for tags on page-load. -Only load jquery if extension is clicked. -Only execute on-click event if SwaggerJacker library is loaded.

--- background.js
+++ background.js
@@ -1,22 +1,37 @@
+// On page load
+// Load SJ library
+chrome.tabs.executeScript( tab, { file:'SwaggerJacker.min.js', allFrames:false }, function(){
 
+	// Fetch Tags
+	// SwaggerJacker.fetchTags ( currentTab.Url );
 
-// Extension Button Click Event
-chrome.browserAction.onClicked.addListener( function( tab ) {
+		// Does page have tags
+		if(SwaggerJacker.Tags.length > 0 ){
+			
+			// Yes
+			// Change extension icon
 
-	// Load jQuery
-    chrome.tabs.executeScript( tab, {file:'jquery.min.js', allFrames:false }, function(){
-	    	
-    		// Show Tagging Interface
-    		// SwaggerJacker.render( currentTab );
+		}
 
-    		// Fetch Tags
-    		// SwaggerJacker.fetchTags ( currentTab.Url );
+		// If library loaded
+		if( typeof ( SwaggerJacker ) == "object" ){
+			// Extension Button Click Event 
+			chrome.browserAction.onClicked.addListener( function( tab ) {
 
-    			// Show Tags
-    			// SwaaggerJacker.showTags( currentTab );
+				// Load jQuery
+			    chrome.tabs.executeScript( tab, { file:'jquery.min.js', allFrames:false }, function(){
 
-    		// Tagging interface should allow user to:
+			    		// Show Tagging Interface
+			    		// SwaggerJacker.render( currentTab );
 
-    			// Create new Tag
-    });
+			    			// Show Tags
+			    			// SwaaggerJacker.showTags( currentTab );
+
+			    		// Tagging interface should allow user to:
+
+			    			// Create new Tag
+			    });
+			});
+		}
+
 });
